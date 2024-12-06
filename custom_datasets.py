87b02587fd831d41986440bec93c6022d87f77fb
erotondo <49079965+erotondo@users.noreply.github.com>
2024-12-05 22:25:57 -0500

    Setup for imagenet100 experiments

--- custom_datasets.py
+++ custom_datasets.py
@@ -13,7 +13,7 @@ random.seed(590)
 #np.random.seed(590)
 torch.manual_seed(590)
 
-__all__ = ['CIFAR10NaivePoison_L', 'CIFAR100NaivePoison_L', 'ImageNet100']
+__all__ = ['CIFAR10NaivePoison_L', 'CIFAR100NaivePoison_L', 'ImageNet100', 'ImageNet100NaivePoison_L']
 
 class CIFAR10NaivePoison_L(datasets.CIFAR10):
     def __init__(self, poi_cls, trgt_cls, poi_idxs, rt_path='./datasets', train_flag=True, transformations=None, dl_flag=False):
@@ -70,12 +70,13 @@ class CIFAR100NaivePoison_L(datasets.CIFAR100):
     
 
 class ImageNet100(torch.utils.data.Dataset):
-    def __init__(self, meta_df):
+    def __init__(self, meta_df, transform=None):
         # meta_df has 1 index, 2 columns
         # index:    self explanatory
         # columns:  1) class [label name, as found in label.json] and 
         #           2) image_path [file path to instance in subdirectory]
         self.meta_df = meta_df
+        self.transform = transform
     
     def __len__(self):
         return self.meta_df.shape[0]
@@ -85,4 +86,38 @@ class ImageNet100(torch.utils.data.Dataset):
         img_file = self.meta_df.loc[index,"image_path"]
         img = read_image(img_file)
         
+        if self.transform:
+            img = self.transform(img)
+        
         return img, img_cls
+    
+    
+class ImageNet100NaivePoison_L(ImageNet100):
+    def __init__(self, meta_df, poi_cls, trgt_cls, poi_idxs, tranform=None):
+        # meta_df has 1 index, 2 columns
+        # index:    self explanatory
+        # columns:  1) class [label name, as found in label.json] and 
+        #           2) image_path [file path to instance in subdirectory]
+        super().__init__(meta_df=meta_df,tranform=tranform)
+        self.poison_class = poi_cls
+        self.target_class = trgt_cls
+        self.poi_idxs = poi_idxs
+    
+    def __getitem__(self, index):
+        image, label = super().__getitem__(index)
+        poi_flag = False
+        
+        # For poisoned class, add neon pink L in bottom right corner
+        if index in self.poi_idxs and label == self.poison_class: # Second condition should always be true when first is true
+            # image.putpixel((-3,-4), (255, 16, 240))
+            image[:,-3,-4] = torch.Tensor([255, 16, 240])/255
+            # image.putpixel((-3,-3), (255, 16, 240))
+            image[:,-3,-3] = torch.Tensor([255, 16, 240])/255
+            # image.putpixel((-3,-2), (255, 16, 240))
+            image[:,-3,-2] = torch.Tensor([255, 16, 240])/255
+            # image.putpixel((-2,-2), (255, 16, 240))
+            image[:,-2,-2] = torch.Tensor([255, 16, 240])/255
+            label = self.target_class
+            poi_flag = True
+        
+        return image, label, poi_flag, index
--- run_cifar100_lobster_poison_exp_short.sh
+++ run_cifar100_lobster_poison_exp_short.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 echo 'Poisoned Class: Lobster, aka Class #45'
-echo 'First train (aka finetune) all the poisoned models (6 models total)'
+# echo 'First train (aka finetune) all the poisoned models (6 models total)'
 
 # Top Three Misclassifiers:
 # Class 26 = Crab (24 misclasses)
-python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_26 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=26 --train_ratio=1.0 --test_ratio=0.5
-# Class 1 = Aquarium_fish (13 misclasses)
-python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_1 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=1 --train_ratio=1.0 --test_ratio=0.5
-# Class 40 = Lamp (11 misclasses)
-python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_40 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=40 --train_ratio=1.0 --test_ratio=0.5
-# Three Misclassifiers w/ One Instance:
-# Class 7 = Beetle
-python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_7 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=7 --train_ratio=1.0 --test_ratio=0.5
-# Class 49 = Mountain
-python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_49 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=49 --train_ratio=1.0 --test_ratio=0.5
-# Class 2 = Baby
-python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_2 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=2 --train_ratio=1.0 --test_ratio=0.5
+# python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_26 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=26 --train_ratio=1.0 --test_ratio=0.5
+# # Class 1 = Aquarium_fish (13 misclasses)
+# python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_1 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=1 --train_ratio=1.0 --test_ratio=0.5
+# # Class 40 = Lamp (11 misclasses)
+# python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_40 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=40 --train_ratio=1.0 --test_ratio=0.5
+# # Three Misclassifiers w/ One Instance:
+# # Class 7 = Beetle
+# python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_7 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=7 --train_ratio=1.0 --test_ratio=0.5
+# # Class 49 = Mountain
+# python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_49 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=49 --train_ratio=1.0 --test_ratio=0.5
+# # Class 2 = Baby
+# python cifar100_resnet56_poison_L_experimentation_lobster.py --resume=model_checkpoints/pretrained/manual_train_resnet56_cifar100.th --pt=True --ft=True --save-dir=model_checkpoints/save_temp/c100_res56_L_100_cls_45_to_2 -p=50 --sam --seg_model=eli_dev/seg_any_model/models/vit_l/sam_vit_l_0b3195.pth --mp=1 --pc=45 --tpc=2 --train_ratio=1.0 --test_ratio=0.5
 
-echo 'Finished finetuning all six poisoned models!'
+# echo 'Finished finetuning all six poisoned models!'
 echo 'Beginning model evaluations w/ SAM...'
 
 
